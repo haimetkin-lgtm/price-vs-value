eb32617f1915aaea871332e1d7132a7500a744fe
Hai Metkin <haimetkin@gmail.com>
2026-06-29 14:44:35 +0300

    add basePath for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,8 @@ const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
   images: { unoptimized: true },
+  basePath: "/price-vs-value",
+  assetPrefix: "/price-vs-value",
 };
 
 export default nextConfig;
